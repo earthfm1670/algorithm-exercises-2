2e07993d3605b0b9d8655dc06e0d416701289bca
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-11-11 11:39:57 +0700

    edit: add additional solution for exercise 3

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,21 @@
 function validatePIN(pin) {
   //Start coding here
+  // 1. วิธีน้ำลายยืด
+  // let numTest;
+  // let lengthTest;
+  // if (parseInt(pin.split("")) === 1) {
+  //   numTest = true;
+  // }
+  // if (pin.split("").length === 4 || pin.split("").length === 6) {
+  //   lengthTest = true;
+  // }
+  // if (numTest && lengthTest) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  // 2. วิธีเขียนภาษาอียิป
   return /^(\d{4}|\d{6})$/.test(pin);
 }
 
